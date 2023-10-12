cbad3a71b9f555fb97852419f81302db1dca5397
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-10-12 17:24:07 -0500

    Add: styles and state function

--- twitter-card/src/TwitterFollowCard.jsx
+++ twitter-card/src/TwitterFollowCard.jsx
@@ -1,6 +1,44 @@
 import { css } from "@emotion/react";
+import { useState } from "react";
 
-export function TwitterFollowCard({ userName, name, isFollowing }) {
+const notFollower = css`
+  all: unset;
+  margin-left: 16px;
+  border: 0;
+  border-radius: 100px;
+  background-color: #fff;
+  color: #000;
+  cursor: pointer;
+  padding: 6px 16px;
+  &:hover {
+    background-color: #c6c6c6;
+    transition: 0.3s ease background-color;
+  }
+`;
+
+const follower = css`
+  all: unset;
+  margin-left: 16px;
+  border: 1px solid #fff;
+  border-radius: 100px;
+  background-color: #010101;
+  color: #fff;
+  cursor: pointer;
+  padding: 6px 16px;
+  &:hover {
+    background-color: #c6c6c6;
+    color: #000;
+    transition: 0.3s ease background-color;
+  }
+`;
+
+export function TwitterFollowCard({ userName, name }) {
+  const [isFollowing, setIsFollowing] = useState(false);
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+  const buttonText = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing ? follower : notFollower;
   return (
     <article
       css={css`
@@ -40,7 +78,9 @@ export function TwitterFollowCard({ userName, name, isFollowing }) {
         </div>
       </header>
       <aside>
-        {isFollowing ? <button>Siguiendo</button> : <button>Seguir</button>}
+        <button css={buttonClassName} onClick={handleClick}>
+          {buttonText}
+        </button>
       </aside>
     </article>
   );
