6886e8c4962cf83668062bb53ea2bd62f9ca3e1e
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-10-12 11:22:04 -0500

    Format styling

--- twitter-card/src/TwitterFollowCard.jsx
+++ twitter-card/src/TwitterFollowCard.jsx
@@ -1,41 +1,48 @@
+import { css } from "@emotion/react";
 
-import { css } from '@emotion/react'
-
-export function TwitterFollowCard( {userName, name, isFollowing}) {
-    return (
-        <article 
-            css={css`
-                display: flex;
-                gap: 10px;
-                justify-content: space-between;
-                margin: 16px;
-                `}>
-          <header
-            css={css`
-            display: flex;
-            gap: 10px;
-            `}>
-            <img css={css`
+export function TwitterFollowCard({ userName, name, isFollowing }) {
+  return (
+    <article
+      css={css`
+        display: flex;
+        gap: 10px;
+        justify-content: space-between;
+        margin: 16px;
+      `}
+    >
+      <header
+        css={css`
+          display: flex;
+          gap: 10px;
+        `}
+      >
+        <img
+          css={css`
             width: 64px;
             border-radius: 100px;
-            `} 
-            src={`https://unavatar.io/${userName}`} alt={`${name}'s avatar`} />
-            <div
-              css={css`
-              color: #fff;
-              `}>
-              <strong>{name}</strong><br />
-              <span 
-              css={css`
+          `}
+          src={`https://unavatar.io/${userName}`}
+          alt={`${name}'s avatar`}
+        />
+        <div
+          css={css`
+            color: #fff;
+          `}
+        >
+          <strong>{name}</strong>
+          <br />
+          <span
+            css={css`
               color: #c6c6c6;
-              `}>
-                {`@${userName}`}
-                </span>
-            </div>
-          </header>
-          <aside>
-            {isFollowing ? <button>Siguiendo</button> : <button>Seguir</button>}
-          </aside>
-        </article>
-        )
-}   
+            `}
+          >
+            {`@${userName}`}
+          </span>
+        </div>
+      </header>
+      <aside>
+        {isFollowing ? <button>Siguiendo</button> : <button>Seguir</button>}
+      </aside>
+    </article>
+  );
+}
