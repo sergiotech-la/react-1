4d47f5319c0847663b51116fe5dd692f4d049c51
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-10-17 10:37:29 -0500

    Add turn handler

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -7,18 +7,35 @@ const TURNS = {
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
-  return <div className={className}>{children}</div>;
+  const handleClick = () => {
+    updateBoard();
+  };
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  };
   return (
     <main className="board">
       <h1>Tic-Tac-Toe</h1>
       <section className="game">
         {board.map((_, index) => {
-          return <Square key={index} index={index}></Square>;
+          return (
+            <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+            ></Square>
+          );
         })}
       </section>
       <section className="turn">
