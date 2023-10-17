3269382dc545d277a2509fff8563fb3e8d0e15a8
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-10-17 12:55:09 -0500

    Add winner combos

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -8,7 +8,7 @@ const TURNS = {
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
   const handleClick = () => {
-    updateBoard();
+    updateBoard(index);
   };
   return (
     <div onClick={handleClick} className={className}>
@@ -16,11 +16,27 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
     </div>
   );
 };
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
-  const updateBoard = () => {
+  const [winner, setWinner] = useState(null);
+  const updateBoard = (index) => {
+    if (board[index]) return;
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
   };
@@ -30,11 +46,9 @@ function App() {
       <section className="game">
         {board.map((_, index) => {
           return (
-            <Square
-              key={index}
-              index={index}
-              updateBoard={updateBoard}
-            ></Square>
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
           );
         })}
       </section>
