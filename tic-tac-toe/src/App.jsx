a940a490a849edaffec1eda04066055eba32b654
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-10-17 10:17:04 -0500

    Add turn visualizer

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,22 +1,28 @@
+import { useState } from "react";
+
 const TURNS = {
   X: "x",
   O: "o",
 };
 
-const board = Array(9).fill(null);
+const Square = ({ children, updateBoard, index }) => {
+  return <div className="square">{children}</div>;
+};
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
   return (
     <main className="board">
       <h1>Tic-Tac-Toe</h1>
       <section className="game">
         {board.map((_, index) => {
-          return (
-            <div className="cell" key={index}>
-              <span className="cell__content">{index}</span>
-            </div>
-          );
+          return <Square key={index} index={index}></Square>;
         })}
+      </section>
+      <section className="turn">
+        <Square>{TURNS.X}</Square>
+        <Square>{TURNS.O}</Square>
       </section>
     </main>
   );
