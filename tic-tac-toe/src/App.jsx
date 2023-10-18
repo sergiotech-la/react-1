4fc8bc1426353c656718f3cc2b6a5f2a3453aefa
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-10-18 14:29:47 -0500

    Add checDraw function

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -48,6 +48,20 @@ function App() {
     return null;
   };
 
+  const notNull = (element) => {
+    return element !== null;
+  };
+
+  const checkDraw = (boardToCheck) => {
+    return boardToCheck.every(notNull);
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
   const updateBoard = (index) => {
     if (board[index] || winner) return;
 
@@ -57,8 +71,11 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
     const newWinner = checkWinner(newBoard);
+    const noWinner = checkDraw(newBoard);
     if (newWinner) {
       setWinner(newWinner);
+    } else if (noWinner) {
+      setWinner(false);
     }
   };
   return (
@@ -77,9 +94,19 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
-      <section>
-        {winner ? <h2>The winner is {winner.toUpperCase()}</h2> : null}
-      </section>
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner === false ? "Draw" : "Winner:"}</h2>
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button onClick={resetGame}>Try again</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
