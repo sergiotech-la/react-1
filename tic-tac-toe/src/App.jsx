f0d8110fe52204e7779a59edb8122f33c78525b3
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-10-19 10:15:14 -0500

    Add localStorage capabilities for saving game status

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -29,8 +29,16 @@ const WINNER_COMBOS = [
 ];
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ? turnFromStorage : TURNS.X;
+  });
   const [winner, setWinner] = useState(null);
 
   //null significa que no existe un ganador aun, false que hay un empate
@@ -61,6 +69,9 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   };
 
   const updateBoard = (index) => {
@@ -71,6 +82,8 @@ function App() {
     setBoard(newBoard);
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
     const newWinner = checkWinner(newBoard);
     const noWinner = checkDraw(newBoard);
     if (newWinner) {
